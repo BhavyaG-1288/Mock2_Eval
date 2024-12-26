4737f117b599a7d8f8e7a4fc81d621a054038286
BhavyaG-1288 <gbhavya2017@gmail.com>
2024-12-26 16:49:22 +0530

    front

--- Frontend/index.js
+++ Frontend/index.js
@@ -3,3 +3,5 @@ import navbar from {./navbar.js}
 document.getElementById("nav")
 
 
+import login from (./loging.js);
+displsy login()
---
+++ Frontend/login.js
@@ -0,0 +1,15 @@
+
+
+
+
+let btn =document.getElementById("btn");
+btn.addEventListener("click", function{
+    let btn =document.getElementById("btn");
+btn.addEventListener("click" function(){
+    if(email&password==0) {
+        alert("Login Success..")
+
+    }else ("Incorrect")
+});
+})
+export login();
